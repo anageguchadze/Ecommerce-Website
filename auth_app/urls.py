14ec06f9ceb35c5992b2f13e25e45b2c44f41699
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-14 18:58:33 +0100

    CHanged settings

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
