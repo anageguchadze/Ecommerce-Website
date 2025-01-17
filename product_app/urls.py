24dfe5315f471fcdd9966c412726721a1eeef2ef
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-17 21:45:48 +0100

    Ann best-seller products to product_app

--- product_app/urls.py
+++ product_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, SubCategoryViewSet, ProductViewSet, NewArrivalsView
+from .views import CategoryViewSet, SubCategoryViewSet, ProductViewSet, NewArrivalsView, BestSellersView
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
@@ -10,4 +10,5 @@ router.register(r'products', ProductViewSet, basename='product')
 urlpatterns = [
     path('', include(router.urls)), 
     path('new-arrivals/', NewArrivalsView.as_view(), name='new_arrivals'), 
+    path('best-sellers/', BestSellersView.as_view(), name='best_sellers'),
 ]
--- product_app/views.py
+++ product_app/views.py
@@ -6,6 +6,10 @@ from rest_framework import generics
 from django.utils import timezone
 from datetime import timedelta
 from rest_framework.filters import SearchFilter
+import random
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -40,3 +44,20 @@ class NewArrivalsView(generics.ListAPIView):
         # Filter products created within the last 30 days
         thirty_days_ago = now - timedelta(days=30)
         return Product.objects.filter(created_at__gte=thirty_days_ago, is_active=True).order_by('-created_at')  # Order by latest first
+    
+
+class BestSellersView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        # Fetch all products
+        products = Product.objects.all()
+        
+        # Handle case where there are fewer than 10 products
+        sample_size = min(len(products), 10)  # Ensure we don't sample more than the available products
+        random_products = random.sample(list(products), k=sample_size)  # Randomly select products
+        
+        # Serialize the selected products
+        serializer = ProductSerializer(random_products, many=True)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
