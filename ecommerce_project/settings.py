e07f5b26f77dd7c963a9454aafab1d06228733aa
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-17 23:05:57 +0100

    fix bugs

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 from datetime import timedelta
 import os
-import dj_database_url
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +81,7 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # This uses SQLite locally
     }
 }
 
